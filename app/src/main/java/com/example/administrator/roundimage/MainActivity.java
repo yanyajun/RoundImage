2d4e077a6fc9eb6be8f5f7f4c8ad911f342a4e53
yanyajun <516288725@qq.com>
2019-01-23 14:39:44 +0800

    round image

---
+++ app/src/main/java/com/example/administrator/roundimage/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.administrator.roundimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView mImageView;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+        mButton = (Button) findViewById(R.id.button);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Glide.with(MainActivity.this).load("http://112.25.69.40/static/data/source_picture/20170811/1502438424209038.jpg").into(mImageView);
+            }
+        });
+    }
+}
